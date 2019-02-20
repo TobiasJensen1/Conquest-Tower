45a05e2ef247b8dc4f98013c2e21e65509b75c46
tobbe <tobbejj@hotmail.com>
2019-02-20 16:51:38 +0100

    basics done

---
+++ Conquest Tower/Assets/Scripts/Npc/NpcMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcMove : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(_navMeshAgent == null)
+        {
+            print("The Nav Mesh Agent Component is Not Attached to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+            
+    }
+    
+    private void SetDestination()
+    {
+        if(_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+}
---
+++ Conquest Tower/Assets/Scripts/PlayerController/TowerPlacer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacer : MonoBehaviour
+{
+
+    Vector3 clickPosition;
+    public GameObject ArcherTower;
+    public GameObject LaserTower;
+    public Camera camera;
+    private GameObject Chosen;
+    Ray placement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                clickPosition = hit.point;
+                print(hit.point);
+                print("hallo");
+                if (hit.point.y < 1f)
+                {
+                    Instantiate(Chosen, hit.point, Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    public void placeLaserTower()
+    {
+        Chosen = LaserTower;
+    }
+
+    public void placeArcherTower()
+    {
+        Chosen = ArcherTower;
+    }
+}
