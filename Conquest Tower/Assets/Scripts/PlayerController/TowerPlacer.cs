c923a22984fb24e7d97b1f6e0ecf61af24d28c0d
saidkayed <36620432+saidkayed@users.noreply.github.com>
2019-02-24 14:12:36 +0100

    tower opt added

--- Conquest Tower/Assets/Scripts/PlayerController/TowerPlacer.cs
+++ Conquest Tower/Assets/Scripts/PlayerController/TowerPlacer.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class TowerPlacer : MonoBehaviour
 {
 
-    Vector3 clickPosition;
+
+    //knapper
+    public GameObject SellBut;
+    public GameObject Upgradebut;
+    //Towers
     public GameObject ArcherTower;
     public GameObject LaserTower;
+    //til klikke kode
+    bool CanBuild = false;
     public Camera camera;
     private GameObject Chosen;
     Ray placement;
+    Vector3 clickPosition;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() { 
+   
+        CanBuild = true;
+      
+        SellBut.SetActive(false);
+        Upgradebut.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,21 +41,40 @@ public class TowerPlacer : MonoBehaviour
                 clickPosition = hit.point;
                 print(hit.point);
                 print("hallo");
-                if (hit.point.y < 1f)
+                if (hit.point.y < 1f && CanBuild)
                 {
                     Instantiate(Chosen, hit.point, Quaternion.identity);
                 }
+
+                if (hit.transform.tag == "Tower")
+                {
+                    //poppe knapper op "upgrade" "Sell" "info om tower"
+                    print("Tower siger hej");
+                    CanBuild = false;
+                    Upgradebut.transform.position = new Vector3(hit.transform.position.x - 20, hit.transform.position.y + 10, hit.transform.position.z);
+                    SellBut.transform.position = new Vector3(hit.transform.position.x + 20, hit.transform.position.y + 10, hit.transform.position.z);
+                    SellBut.SetActive(true);
+                    Upgradebut.SetActive(true);
+
+                }
+                else if(hit.transform.tag == "Untagged")
+                {
+                SellBut.SetActive(false);
+                Upgradebut.SetActive(false);
+                CanBuild = true;
+            }
             }
         }
-    }
 
-    public void placeLaserTower()
-    {
-        Chosen = LaserTower;
     }
+        public void placeLaserTower()
+        {
+            Chosen = LaserTower;
+        }
 
-    public void placeArcherTower()
-    {
-        Chosen = ArcherTower;
-    }
+        public void placeArcherTower()
+        {
+            Chosen = ArcherTower;
+        }
+    
 }
