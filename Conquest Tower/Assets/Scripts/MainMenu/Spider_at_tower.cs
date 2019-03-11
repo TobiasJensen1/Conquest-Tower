b851f91aa46cb91f32ac4daed4858903d24152f0
saidkayed <36620432+saidkayed@users.noreply.github.com>
2019-03-11 02:08:19 +0100

    main menu update

---
+++ Conquest Tower/Assets/Scripts/MainMenu/Spider_at_tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider_at_tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Animation>().Play("idle");
+        StartCoroutine("PlayAnimation");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        while (true)
+        {
+            gameObject.GetComponent<Animation>().Play("attack1");
+            yield return new WaitForSeconds(1);
+            gameObject.GetComponent<Animation>().Play("idle");
+            yield return new WaitForSeconds(4);
+
+        }
+    }
+}
